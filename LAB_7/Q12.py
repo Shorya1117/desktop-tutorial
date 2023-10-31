d90a74a5abdfd685587e8223300c82c5c88534c8
Shorya1117 <143099970+Shorya1117@users.noreply.github.com>
2023-11-01 03:18:40 +0530

    Add files via upload

---
+++ LAB_7/Q12.py
@@ -0,0 +1,26 @@
+# N=int(input("enter string")) 
+# horizontal_line = " +/" +"\/" *(N-2) +"+"
+# vertical_line = " |" +"  "*(N-2) +" |"
+
+# for i in range(N):
+#         if i == 0 or i == N - 1:
+#             print(horizontal_line)
+#         else:
+#             print(vertical_line)
+
+# cook your dish here
+t=int(input())
+count=0
+count_1=0
+for i in range(t):
+    a1,a2,a3,a4,a5,a6,a7=map(int,input().split())
+    list=[a1,a2,a3,a4,a5,a6,a7]
+    for k in range(7):
+        if list[k]==1:
+            count=count+1
+        elif list[k]==0:
+            count_1+=1
+    if count>count_1:
+        print("yes")
+    else:
+        print("no")
---
+++ LAB_7/Q4.py
@@ -0,0 +1,6 @@
+n=str(input("enter the sentence:"))
+m=n[::-1]
+if m==n:
+    print("no. is pallindrome")
+else:
+    print("not  pallidrome")
